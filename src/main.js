2e9a451bf583bda3c4b961df456de840e643b22e
Bertil <30950082@qq.com>
2022-06-01 00:47:07 +0800

    chore: add header

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import cookieParser from 'cookie-parser';
 import fs from 'fs';
 
 let app = express();
+app.all('*',function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+	res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+	if (req.method == 'OPTIONS') {
+		res.send(200);
+	}
+	else {
+		next();
+	}
+});
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
